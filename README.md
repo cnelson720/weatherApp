fa51c13454d2c8ff51e5628bcf7c26eff03fa423
Chris N <chris.nelson.dev@gmail.com>
2022-03-16 11:32:43 -0600

    v2

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # weatherApp
-iOS-like weather apps using open weather API. One for general temperature and weather and another with snow fall volume and weather.
-
-currently does not actually locate city name. it finds lat/long but cant get a city name out of that
+iOS-like weather apps using open weather API. One for general temperature and weather and another with snow fall volume and weather
--- script.js
+++ script.js
@@ -12,8 +12,18 @@ const forecast5 = document.getElementById('5');
 const forecast6 = document.getElementById('6');
 const forecast7 = document.getElementById('7');
 
-const API_KEY = '109d6fc9488b0e09125f891fd49dbe5b';
+//detailed info
+const dTempDiv = document.getElementById('detailedTemp');
+const feelsLikeDiv = document.getElementById('feels-like');
+const cloudsDiv = document.getElementById('clouds');
+const sunsetDiv = document.getElementById('sunset');
+const sunriseDiv = document.getElementById('sunrise');
+const humidityDiv = document.getElementById('humidity');
+const cloudPercDiv = document.getElementById('cloudPerc');
+const windSpeedDiv = document.getElementById('wind_speed');
+const uviDiv = document.getElementById('uvi');
 
+const API_KEY = '109d6fc9488b0e09125f891fd49dbe5b';
 
 function getWeatherData(){
     navigator.geolocation.getCurrentPosition((success) =>{
@@ -24,9 +34,9 @@ function getWeatherData(){
 
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
             console.log(data);
-
             showWeatherData(data);
             showSnowData(data);
+            showDetailedData(data);
         })
     })
 }
@@ -66,22 +76,54 @@ function showSnowData(data){
         forecast.innerText += '\n' + descriptions[i] + ' · ' + snowfall + '"' + "  ";
 
         if (descriptions[i].includes('rain')){
-            forecast.innerHTML += "<i class='bx bx-cloud-light-rain' ></i>";
+            forecast.innerHTML += "<i id='rain' class='bx bx-cloud-light-rain' ></i>";
         }
         if (descriptions[i].includes('snow')){
-            forecast.innerHTML += "<i class='bx bx-cloud-snow' ></i>";
+            forecast.innerHTML += "<i id='snow' class='bx bx-cloud-snow' ></i>";
         }
 
         if (descriptions[i].includes('cloud')){
-            forecast.innerHTML += "<i class='bx bx-cloud' ></i>";
+            forecast.innerHTML += "<i id='cloudy' class='bx bx-cloud' ></i>";
         }
         
         if (descriptions[i].includes('sky' || 'sun')){
-            forecast.innerHTML += "<i class='bx bx-sun' ></i>";
+            forecast.innerHTML += "<i id='clear' class='bx bx-sun' ></i>";
         }
     }
 
 }
 
-getWeatherData();
+function showDetailedData(data){
+    dTempDiv.innerText = Math.round(data.current.temp) + '°';
+    feelsLikeDiv.innerText = Math.round(data.current.feels_like) + '°';
 
+    let riseUnix = data.current.sunrise;
+    let setUnix = data.current.sunset;
+
+    let sunriseTime = new Date(riseUnix * 1000);
+    let sunsetTime = new Date(setUnix * 1000);
+
+    function formatTime(date){
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+
+        var ampm = hours >= 12 ? 'pm' : 'am';
+
+        hours %= 12;
+        hours = hours ? hours : 12;
+
+        minutes = minutes < 10 ? '0'+ minutes: minutes;
+
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+
+    sunriseDiv.innerText = formatTime(sunriseTime);
+    sunsetDiv.innerText = formatTime(sunsetTime);
+
+    humidityDiv.innerText = data.current.humidity + "%";
+    cloudPercDiv.innerText = data.current.clouds + "%";
+    windSpeedDiv.innerText = Math.floor(data.current.wind_speed);
+    uviDiv.innerText = data.current.uvi;
+
+}
